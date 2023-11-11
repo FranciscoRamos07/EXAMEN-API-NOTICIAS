a9c7fa1ebdbbd847767334608add8bb9b30ccb49
Francisco Ramos <franciscoramos0709@gmail.com>
2023-11-10 18:36:56 -0700

    UltimaVersion

--- src/App.js
+++ src/App.js
@@ -1,297 +1,143 @@
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Axios from "axios";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import Swal from 'sweetalert2'
+//import Swal from 'sweetalert2'
 
 function App() {
-  const [nombre, setNombre] = useState("");
-  const [edad, setEdad] = useState();
-  const [pais, setPais] = useState("");
-  const [puesto, setPuesto] = useState("");
-  const [anos, setAnos] = useState();
-  const [id, setId] = useState();
+ 
  
 
-  const [editar,setEditar] = useState(false);
-
-  const [empleadosList, setEmpleados] = useState([]);
-
-  const add = () => {
-    Axios.post("http://localhost:3001/create", {
-      nombre: nombre,
-      edad: edad,
-      pais: pais,
-      puesto: puesto,
-      anos: anos,
-    }).then(() => {
-      getEmpleados();
-      limpiarCampos();
-      Swal.fire({
-        title: "<strong>Registro Exitoso!</strong>",
-        html: "<i>el empleado <strong>" + nombre + "</strong> fue registrado con exito!</i>",
-        icon: 'success',
-        timer: 2000
-      })
-    });
-  };
-
-
-  const update = () => {
-    Axios.put("http://localhost:3001/update", {
-      id: id,
-      nombre: nombre,
-      edad: edad,
-      pais: pais,
-      puesto: puesto,
-      anos: anos,
-    }).then(() => {
-      getEmpleados();
-      limpiarCampos();
-      Swal.fire({
-        title: "<strong>Actualizacion Exitosa!</strong>",
-        html: "<i>el empleado <strong>" + nombre + "</strong> fue Actualizado con exito!</i>",
-        icon: 'success',
-        timer: 2000
-      })
-    });
-  };
-
-
-
-  const deleteEmple = (val) => {
-
-
-    Swal.fire({
-      title: 'Confirmar Eliminado',
-      html: "<i>Deseas eliminar a <strong>" + val.nombre + "</strong>???</i>",
-      icon: 'warning',
-      showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Si, Eliminar!'
-    }).then((result) => {
-      if (result.isConfirmed) {
-
-        Axios.delete(`http://localhost:3001/delete/${val.id}`).then(() => {
-          getEmpleados();
-          limpiarCampos();
-          Swal.fire(
-            'Eliminado!',
-            val.nombre+' fue Eliminado.',
-            'success',
-
-          )
-        });
-        
-      }
-    })
-
-
-
-
-  };
-
-  const limpiarCampos = () => {
-    setAnos("");
-    setNombre("");
-    setPuesto("");
-    setEdad("");
-    setPais("");
-    setId("");
-    setEditar(false);
-  }
-
-  const editarEmpleado = (val)=>{
-    setEditar(true);
-
-    setNombre(val.nombre);
-    setEdad(val.edad);
-    setPuesto(val.puesto);
-    setPais(val.pais);
-    setAnos(val.anos);
-    setId(val.id);
-   
-  }
-
-  const getEmpleados = () => {
-    Axios.get("http://localhost:3001/empleados").then((response) => {
-      setEmpleados(response.data);
-    });
-  }
-
-
-
   
+
+  const [noticiasList, setNoticias] = useState([]);
+  const [search, setSearch] = useState("");
+
+
+  const getNoticias = ()  => {
+    Axios.get("http://localhost:3001/noticias").then((response) => {
+      setNoticias(response.data);
+    });
+  }
+
+  const searcher =(e)=>{
+    setSearch(e.target.value)
+    console.log(e.target.value)
+  }
+
+
+
+  let results = []
+  if(!search){
+    results = noticiasList
+
+
+  }else{
+
+    results = noticiasList.filter((dato)=>
+    dato.zona.toLowerCase().includes(search.toLocaleLowerCase()))
+  }
+
+
+  useEffect(()=>{getNoticias()},[])
+
+
+
+
+
+
   
  
 
 
 
   return (
+<div className="background">
     <div className="container">
- 
+            
+        <nav className="navbar  border-bottom border-body navbar-expand-lg">
+          <div className="container-fluid">
+            <a className="navbar-brand navText" onClick={getNoticias}>NOTICIAS STORM</a>
 
-      <div className="card text-center">
-        <div className="card-header">Gestion de empleados</div>
-        <div className="card-body">
-          <div className="input-group mb-3">
-            <span className="input-group-text" id="basic-addon1">
-              Nombre:{""}
-            </span>
-            <input
-              type="text"
-              onChange={(event) => {
-                setNombre(event.target.value);
-              }}
-              className="form-control"
-              value={nombre}
-              placeholder="Ingrese Un Nombre"
-              aria-label="Username"
-              aria-describedby="basic-addon1"
-            />
-          </div>
-
-          <div className="input-group mb-3">
-            <span className="input-group-text" id="basic-addon1">
-              Edad:{""}
-            </span>
-            <input
-              type="number"
-              value={edad}
-              onChange={(event) => {
-                setEdad(event.target.value);
-              }}
-              className="form-control"
-              placeholder="Ingrese Edad"
-              aria-label="Username"
-              aria-describedby="basic-addon1"
-            />
-          </div>
-
-          <div className="input-group mb-3">
-            <span className="input-group-text" id="basic-addon1">
-              Pais:{""}
-            </span>
-            <input
-              type="text"
-              value={pais}
-              onChange={(event) => {
-                setPais(event.target.value);
-              }}
-              className="form-control"
-              placeholder="Ingrese Nacionalidad"
-              aria-label="Username"
-              aria-describedby="basic-addon1"
-            />
-          </div>
-
-          <div className="input-group mb-3">
-            <span className="input-group-text" id="basic-addon1">
-              Puesto:{""}
-            </span>
-            <input
-              type="text"
-              value={puesto}
-              onChange={(event) => {
-                setPuesto(event.target.value);
-              }}
-              className="form-control"
-              placeholder="Ingrese Puesto"
-              aria-label="Username"
-              aria-describedby="basic-addon1"
-            />
-          </div>
-
-          <div className="input-group mb-3">
-            <span className="input-group-text" id="basic-addon1">
-              Años de experiencia:{""}
-            </span>
-            <input
-              type="number"
-              value={anos}
-              onChange={(event) => {
-                setAnos(event.target.value);
-              }}
-              className="form-control"
-              placeholder="Ingrese los Años de experiencia "
-              aria-label="Username"
-              aria-describedby="basic-addon1"
-            />
-          </div>
-        </div>
-        <div className="card-footer text-body-secondary">
-
-          {
-            editar == true?
-            <div>
-              <button className="btn btn-warning m-2" onClick={update}>Actualizar</button> 
-              <button className="btn btn-danger m-2" onClick={limpiarCampos}>Cancelar</button> 
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <a className="nav-link active navText" aria-current="page" href="#">Home</a>
+                </li>
+              </ul>
+              <form className="d-flex navSearch" role="search">
+                <input value={search} onChange={searcher} className="form-control me-2" type="search" placeholder="Buscar" aria-label="Search"/>
+                <button className="btn btn-outline-light" type="submit">Buscar</button>
+              </form>
             </div>
-          :
-          <button className="btn btn-success" onClick={add}>Registrar</button>
-           
+          </div>
+        </nav>
 
 
-          }
-                <br/>
-                <br/>
-          <button className="btn btn-dark" onClick={getEmpleados}>
-            Listar
-          </button>
+
+
+
+
+
+
+
+
+
+
+      
+
+
+{results.map((val,key) => {
+          return(
+<div className="contenedores">
+  <div className="row">
+            {/* <img key={val.id} src={val.imagen} alt=""></img> */}
+
+    <div className="col-xl-6 col-sm-2">
+      <div className="card cardDisplay" style={{width: '20rem'}}>
+          <img key={val.id} src={val.imagen} className="card-img-top" alt="avatar"/>
+        <div className="card-body">
+          <div className="subject-txt">
+          <h5 className="card-title title-txt">{val.topic}</h5>
+          <p className="card-text">{val.subject}</p>
           
+      <div className="botton">
+          <p className="card-text">{val.zona}</p>
+          <p className="card-text">{val.fecha}</p>
+
+        <a href="https://www.forbes.com/?sh=29d31ba2254c" className="btn btn-dark">Mas informacion</a>
+          </div>
+            </div>
+
         </div>
       </div>
-{/* ----------------tabla---------------- */}
-              <table className="table">
-            <thead>
-              <tr>
-                <th scope="col">#</th>
-                <th scope="col">Nombre</th>
-                <th scope="col">Edad</th>
-                <th scope="col">Pais</th>
-                <th scope="col">Puesto</th>
-                <th scope="col">Experiencia</th>
-                <th scope="col">Acciones</th>
-              </tr>
-            </thead>
-            <tbody>
 
-            {empleadosList.map((val,key) => {
-            return (
-            <tr key={val.id}>
-            <th >{val.id}</th>
-            <td>{val.nombre}</td>
-            <td>{val.edad}</td>
-            <td>{val.pais}</td>
-            <td>{val.puesto}</td>
-            <td>{val.anos}</td>
-            <td>
-            <div class="btn-group" role="group" aria-label="Basic example">
-              <button type="button"
-              onClick={()=>{
-                editarEmpleado(val)
-              }}
-              
-              class="btn btn-info">Editar</button>
-              <button type="button" 
-              onClick={()=>{
-                deleteEmple(val);
-              }}
-              class="btn btn-danger">Eliminar</button>
+          </div>
             </div>
-            </td>
-            </tr>
-            )
-            })}
-
-            </tbody>
-        </table>
+            </div>
+          )})}
 
 
-    </div>
+
+
+
+{/* ----------------tabla---------------- */}
+
+
+ {/* <button className="btn btn-dark" onClick={getNoticias}>
+            Listar
+          </button>  */}
+
+
+        
+ 
+
+
+
+  </div>
+</div>
   );
 }
 
